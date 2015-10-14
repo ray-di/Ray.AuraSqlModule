e61be9d0fb08a93adf35a731b9c44e98dcd00322
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-10-14 19:19:44 +0900

    extract test method

--- tests/Pagerfanta/AuraSqlPagerModuleTest.php
+++ tests/Pagerfanta/AuraSqlPagerModuleTest.php
@@ -5,7 +5,6 @@ use Ray\Di\Injector;
 
 class AuraSqlPagerModuleTest extends AbstractPdoTestCase
 {
-
     public function testNewInstance()
     {
         $factory = (new Injector(new AuraSqlPagerModule()))->getInstance(AuraSqlPagerFactoryInterface::class);
@@ -13,6 +12,16 @@ class AuraSqlPagerModuleTest extends AbstractPdoTestCase
         $this->assertInstanceOf(AuraSqlPagerFactory::class, $factory);
         $sql = 'SELECT * FROM posts';
         $pager = $factory->newInstance($this->pdo, $sql, 1, '/?page={page}&category=sports');
+        $this->assertInstanceOf(AuraSqlPager::class, $pager);
+
+        return $pager;
+    }
+
+    /**
+     * @depends testNewInstance
+     */
+    public function testExecute(AuraSqlPager $pager)
+    {
         $user = $pager->execute([], 2);
         $this->assertTrue($user->hasNext);
         $this->assertTrue($user->hasPrevious);
@@ -24,7 +33,7 @@ class AuraSqlPagerModuleTest extends AbstractPdoTestCase
                 ],
         ];
         $this->assertSame($expected, $user->data);
-        $expected = '<nav><a href="/?page=1">Previous</a><a href="/?page=1">1</a><span class="current">2</span><a href="/?page=3">3</a><a href="/?page=4">4</a><a href="/?page=5">5</a><span class="dots">...</span><a href="/?page=50">50</a><a href="/?page=3">Next</a></nav>';
+        $expected = '<nav><a href="/?page=1&category=sports">Previous</a><a href="/?page=1&category=sports">1</a><span class="current">2</span><a href="/?page=3&category=sports">3</a><a href="/?page=4&category=sports">4</a><a href="/?page=5&category=sports">5</a><span class="dots">...</span><a href="/?page=50&category=sports">50</a><a href="/?page=3&category=sports">Next</a></nav>';
         $this->assertSame($expected, $user->html);
     }
 }
