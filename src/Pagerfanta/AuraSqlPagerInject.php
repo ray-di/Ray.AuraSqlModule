ac55debcc057240dab8ff2667ca7f3aa0f0224de
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-10-21 05:21:52 +0900

    added pager inject trait

---
+++ src/Pagerfanta/AuraSqlPagerInject.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the Ray.AuraSqlModule package
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace Ray\AuraSqlModule;
+
+use Ray\AuraSqlModule\Pagerfanta\AuraSqlPagerFactoryInterface;
+
+trait AuraSqlPagerInject
+{
+    /**
+     * @var AuraSqlPagerFactoryInterface
+     */
+    protected $pagerFactory;
+
+    /**
+     * @param AuraSqlPagerFactoryInterface $pagerFactory
+     *
+     * @\Ray\Di\Di\Inject
+     */
+    public function setAuraSqlPager(AuraSqlPagerFactoryInterface $pagerFactory)
+    {
+        $this->$pagerFactory = $pagerFactory;
+    }
+}
