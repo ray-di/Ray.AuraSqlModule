e17acb3c8795c52edcd201a043cdea63f3ea56d2
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-02-21 21:59:48 +0900

    AuraSqlModule take $slave for m/s replication

--- tests/AuraSqlModuleTest.php
+++ tests/AuraSqlModuleTest.php
@@ -2,6 +2,7 @@
 
 namespace Ray\AuraSqlModule;
 
+use Aura\Sql\ConnectionLocatorInterface;
 use Aura\Sql\ExtendedPdo;
 use Aura\Sql\ExtendedPdoInterface;
 use Ray\Compiler\DiCompiler;
@@ -18,5 +19,18 @@ class AuraSqlModuleTest extends \PHPUnit_Framework_TestCase
     public function testCompile()
     {
         (new DiCompiler(new AuraSqlModule('sqlite::memory:'), $_ENV['TMP_DIR']))->compile();
+    }
+
+    public function testSlaveModule()
+    {
+        $module = new AuraSqlModule('mysql:host=localhost;dbname=testdb', 'root', '', 'slave1,slave2');
+        $instance = $module->getContainer()->getContainer()['Aura\Sql\ConnectionLocatorInterface-'];
+        /* @var $instance \Ray\Di\Instance */
+        /* @var $locator ConnectionLocatorInterface */
+        $locator = $instance->value;
+        $this->assertInstanceOf(ConnectionLocatorInterface::class, $locator);
+        $read = $locator->getRead();
+        $dsn = $read->getDsn();
+        $this->assertContains($dsn, ['mysql:host=slave1;dbname=testdb', 'mysql:host=slave2;dbname=testdb']);
     }
 }
