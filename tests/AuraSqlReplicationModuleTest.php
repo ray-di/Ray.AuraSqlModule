9c852a3f199d751e1b077d30ec5527e46d2be9f8
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-02-22 19:16:21 +0900

    AuraSqlReplicationModule takes qualifier in 2nd parameter

--- tests/AuraSqlReplicationModuleTest.php
+++ tests/AuraSqlReplicationModuleTest.php
@@ -4,6 +4,7 @@ namespace Ray\AuraSqlModule;
 
 use Aura\Sql\ConnectionLocator;
 use Aura\Sql\ExtendedPdo;
+use Aura\Sql\ExtendedPdoInterface;
 use Ray\Di\Injector;
 
 class AuraSqlReplicationModuleTest extends \PHPUnit_Framework_TestCase
@@ -58,5 +59,21 @@ class AuraSqlReplicationModuleTest extends \PHPUnit_Framework_TestCase
         $model = (new Injector(new AuraSqlReplicationModule($locator), $_ENV['TMP_DIR']))->getInstance(FakeRepModel::class);
         $this->assertInstanceOf(ExtendedPdo::class, $model->pdo);
         $this->assertSame($masterPdo, $model->pdo);
+    }
+
+    /**
+     * @dataProvider connectionProvider
+     */
+    public function testLocatorMasterWithQualifer(ConnectionLocator $locator, ExtendedPdo $masterPdo, ExtendedPdo $slavePdo)
+    {
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        /* @var  $db1Master ExtendedPdo */
+        /* @var  $db2Master ExtendedPdo */
+        list(list($locator2)) = $this->connectionProvider();
+        $db1Master = (new Injector(new AuraSqlReplicationModule($locator, 'db1'), $_ENV['TMP_DIR']))->getInstance(ExtendedPdoInterface::class, 'db1');
+        $db2Master = (new Injector(new AuraSqlReplicationModule($locator2, 'db2'), $_ENV['TMP_DIR']))->getInstance(ExtendedPdoInterface::class, 'db2');
+        $this->assertInstanceOf(ExtendedPdo::class, $db1Master);
+        $this->assertInstanceOf(ExtendedPdo::class, $db2Master);
+        $this->assertNotSame($db1Master, $db2Master);
     }
 }
