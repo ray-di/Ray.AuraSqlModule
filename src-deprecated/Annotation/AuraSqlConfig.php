eeddf89d1ba283e706b3d60c49a8c4964826967f
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-01-24 19:12:07 +0900

    Move deprecated annotation

--- src-deprecated/Annotation/AuraSqlConfig.php
+++ src-deprecated/Annotation/AuraSqlConfig.php
@@ -12,14 +12,18 @@ use Ray\Di\Di\Qualifier;
  * @Annotation
  * @Target("METHOD")
  * @Qualifier
+ * @deprecated -- No one using?
  */
 #[Attribute(Attribute::TARGET_METHOD), Qualifier]
-final class Write implements NamedArgumentConstructorAnnotation
+final class AuraSqlConfig implements NamedArgumentConstructorAnnotation
 {
-    /** @var string */
+    /** @var ?array<string> */
     public $value;
 
-    public function __construct(string $value)
+    /**
+     * @param array<string> $value
+     */
+    public function __construct(?array $value = null)
     {
         $this->value = $value;
     }
--- src-deprecated/Annotation/Write.php
+++ src-deprecated/Annotation/Write.php
@@ -12,17 +12,15 @@ use Ray\Di\Di\Qualifier;
  * @Annotation
  * @Target("METHOD")
  * @Qualifier
+ * @deprecated -- No one using?
  */
 #[Attribute(Attribute::TARGET_METHOD), Qualifier]
-final class AuraSqlConfig implements NamedArgumentConstructorAnnotation
+final class Write implements NamedArgumentConstructorAnnotation
 {
-    /** @var ?array<string> */
+    /** @var string */
     public $value;
 
-    /**
-     * @param array<string> $value
-     */
-    public function __construct(?array $value = null)
+    public function __construct(string $value)
     {
         $this->value = $value;
     }
