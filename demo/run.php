19ee092d538627d59b18dd4dd6861b77493752b0
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-01-24 23:29:43 +0900

    Remove demos

--- demo/run.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-passthru('php ' . __DIR__ . '/01-constructor_injection.php');
-passthru('php ' . __DIR__ . '/02-setter_injection.php');
-passthru('php ' . __DIR__ . '/03-replication.php');
-passthru('php ' . __DIR__ . '/04-transaction.php');
